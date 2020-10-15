744032635fead9d067910dff13503ffd78ef4879
christian <xhrist14n@gmail.com>
2020-10-15 15:40:16 -0500

    app

---
+++ one/src/main/java/one/app/App.java
@@ -0,0 +1,15 @@
+package one.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Iniciando App" );
+
+        System.out.println( "Finalizo App" );
+    }
+}
