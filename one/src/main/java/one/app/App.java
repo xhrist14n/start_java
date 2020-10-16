2e42e5eb2de18b9d4c2d51cc9aa499f63320299a
christian <xhrist14n@gmail.com>
2020-10-16 01:02:52 -0500

    app

--- one/src/main/java/one/app/App.java
+++ one/src/main/java/one/app/App.java
@@ -10,9 +10,20 @@ public class App
     {
         System.out.println( "Iniciando App" );
 
-        DialogoEntrada entrada = new DialogoEntrada();
+        DialogoEntrada dialogo_entrada= new DialogoEntrada();
 
-        Double decimal = entrada.ingresarDecimal("Ingrese un decimal");
+        Entrada entrada = new Entrada();
+
+        Double decimal = dialogo_entrada.ingresarDecimal("Ingrese un decimal");
+
+        String nombre = dialogo_entrada.ingresarDato("Ingrese un nombre");
+
+        boolean valido = entrada.validarNombre(nombre);
+
+        while(valido == false){
+            nombre = dialogo_entrada.ingresarDato("Ingrese un nombre");
+            valido = entrada.validarNombre(nombre);
+        }
 
         System.out.println( "Finalizo App" );
     }
---
+++ three/src/main/java/three/app/App.java
@@ -0,0 +1,15 @@
+package three.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Iniciando App" );
+
+        System.out.println( "Finalizando App" );
+    }
+}
